b1b8f24f5b9e467c459564a1668f698c32a05f4b
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2019-01-29 21:23:10 -0600

    Added Inventory Entry code.

--- RE2REmakeSRT/GameMemory.cs
+++ RE2REmakeSRT/GameMemory.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,14 +19,36 @@ namespace RE2REmakeSRT
         private const string IGT_TIMESPAN_STRING_FORMAT = @"hh\:mm\:ss\.fff";
 
         // Pointers
-        public long BaseAddress { get; private set;}
+        public long BaseAddress { get; private set; }
         public MultilevelPointer PointerIGT { get; private set; }
         public MultilevelPointer PointerPlayerHP { get; private set; }
-        public MultilevelPointer PointerBossHP { get; private set; }
+        public MultilevelPointer PointerEnemyHP { get; private set; }
+        public MultilevelPointer PointerInventoryEntry1 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry2 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry3 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry4 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry5 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry6 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry7 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry8 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry9 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry10 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry11 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry12 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry13 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry14 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry15 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry16 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry17 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry18 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry19 { get; private set; }
+        public MultilevelPointer PointerInventoryEntry20 { get; private set; }
+
 
         // Values
         public int PlayerCurrentHealth { get; private set; }
         public int PlayerMaxHealth { get; private set; }
+        public InventoryEntry[] PlayerInventory { get; private set; }
         public int BossCurrentHealth { get; private set; }
         public int BossMaxHealth { get; private set; }
 
@@ -60,10 +83,31 @@ namespace RE2REmakeSRT
             this.BaseAddress = this.proc.MainModule.BaseAddress.ToInt64();
             this.PointerIGT = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACAE0, 0x2E0, 0x218, 0x610, 0x710, 0x60);
             this.PointerPlayerHP = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x20);
-            this.PointerBossHP = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x0707B758, 0x80, 0x88, 0x18, 0x1A0);
+            this.PointerEnemyHP = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x0707B758, 0x80, 0x88, 0x18, 0x1A0);
+            this.PointerInventoryEntry1 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x20, 0x18, 0x10);
+            this.PointerInventoryEntry2 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x28, 0x18, 0x10);
+            this.PointerInventoryEntry3 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x30, 0x18, 0x10);
+            this.PointerInventoryEntry4 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x38, 0x18, 0x10);
+            this.PointerInventoryEntry5 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x40, 0x18, 0x10);
+            this.PointerInventoryEntry6 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x48, 0x18, 0x10);
+            this.PointerInventoryEntry7 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x50, 0x18, 0x10);
+            this.PointerInventoryEntry8 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x58, 0x18, 0x10);
+            this.PointerInventoryEntry9 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x60, 0x18, 0x10);
+            this.PointerInventoryEntry10 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x68, 0x18, 0x10);
+            this.PointerInventoryEntry11 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x70, 0x18, 0x10);
+            this.PointerInventoryEntry12 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x78, 0x18, 0x10);
+            this.PointerInventoryEntry13 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x80, 0x18, 0x10);
+            this.PointerInventoryEntry14 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x88, 0x18, 0x10);
+            this.PointerInventoryEntry15 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x90, 0x18, 0x10);
+            this.PointerInventoryEntry16 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0x98, 0x18, 0x10);
+            this.PointerInventoryEntry17 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0xA0, 0x18, 0x10);
+            this.PointerInventoryEntry18 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0xA8, 0x18, 0x10);
+            this.PointerInventoryEntry19 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0xB0, 0x18, 0x10);
+            this.PointerInventoryEntry20 = new MultilevelPointer(memoryAccess, this.BaseAddress + 0x070ACA88, 0x50, 0x98, 0x10, 0xB8, 0x18, 0x10);
 
             this.PlayerCurrentHealth = 0;
             this.PlayerMaxHealth = 0;
+            this.PlayerInventory = new InventoryEntry[20];
             this.BossCurrentHealth = 0;
             this.BossMaxHealth = 0;
         }
@@ -77,7 +121,7 @@ namespace RE2REmakeSRT
                     {
                         this.PointerIGT.UpdatePointers();
                         this.PointerPlayerHP.UpdatePointers();
-                        this.PointerBossHP.UpdatePointers();
+                        this.PointerEnemyHP.UpdatePointers();
 
                         break;
                     }
@@ -110,8 +154,8 @@ namespace RE2REmakeSRT
                         this.PlayerCurrentHealth = this.PointerPlayerHP.DerefInt(0x58);
 
                         // Boss HP
-                        this.BossMaxHealth = this.PointerBossHP.DerefInt(0x58);
-                        this.BossCurrentHealth = this.PointerBossHP.DerefInt(0x54);
+                        this.BossMaxHealth = this.PointerEnemyHP.DerefInt(0x58);
+                        this.BossCurrentHealth = this.PointerEnemyHP.DerefInt(0x54);
 
                         break;
                     }
@@ -137,7 +181,30 @@ namespace RE2REmakeSRT
                         RefreshSlim();
 
                         // Other lookups that don't need to update as often.
-                        // ...
+                        // Inventory
+                        PlayerInventory[0] = new InventoryEntry(this.PointerInventoryEntry1.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[1] = new InventoryEntry(this.PointerInventoryEntry2.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[2] = new InventoryEntry(this.PointerInventoryEntry3.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[3] = new InventoryEntry(this.PointerInventoryEntry4.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[4] = new InventoryEntry(this.PointerInventoryEntry5.DerefByteArray(-0x90, 0xF0));
+
+                        PlayerInventory[5] = new InventoryEntry(this.PointerInventoryEntry6.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[6] = new InventoryEntry(this.PointerInventoryEntry7.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[7] = new InventoryEntry(this.PointerInventoryEntry8.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[8] = new InventoryEntry(this.PointerInventoryEntry9.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[9] = new InventoryEntry(this.PointerInventoryEntry10.DerefByteArray(-0x90, 0xF0));
+
+                        PlayerInventory[10] = new InventoryEntry(this.PointerInventoryEntry11.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[11] = new InventoryEntry(this.PointerInventoryEntry12.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[12] = new InventoryEntry(this.PointerInventoryEntry13.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[13] = new InventoryEntry(this.PointerInventoryEntry14.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[14] = new InventoryEntry(this.PointerInventoryEntry15.DerefByteArray(-0x90, 0xF0));
+
+                        PlayerInventory[15] = new InventoryEntry(this.PointerInventoryEntry16.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[16] = new InventoryEntry(this.PointerInventoryEntry17.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[17] = new InventoryEntry(this.PointerInventoryEntry18.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[18] = new InventoryEntry(this.PointerInventoryEntry19.DerefByteArray(-0x90, 0xF0));
+                        PlayerInventory[19] = new InventoryEntry(this.PointerInventoryEntry20.DerefByteArray(-0x90, 0xF0));
 
                         break;
                     }
---
+++ RE2REmakeSRT/InventoryEntry.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace RE2REmakeSRT
+{
+    [DebuggerDisplay("{_DebuggerDisplay,nq}")]
+    public struct InventoryEntry
+    {
+        /// <summary>
+        /// Debugger display message.
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public string _DebuggerDisplay
+        {
+            get
+            {
+                if (IsItem)
+                    return string.Format("[#{0}] Item {1} Quantity {2}", SlotPosition, ItemID, Quantity);
+                else if (IsWeapon)
+                    return string.Format("[#{0}] Weapon {1} Quantity {2} Attachments {3}", SlotPosition, WeaponID, Quantity, Attachments);
+                else
+                    return string.Format("[#{0}] Empty Slot", SlotPosition);
+            }
+        }
+
+        // Private storage variable.
+        private readonly byte[] data; // 240 (0xF0) bytes.
+
+        // Public accessor properties.
+        public int SlotPosition => ProcessMemory.HighPerfBitConverter.ToInt32(data, 0x58);
+        public int ItemID => ProcessMemory.HighPerfBitConverter.ToInt32(data, 0xA0);
+        public int WeaponID => ProcessMemory.HighPerfBitConverter.ToInt32(data, 0xA4);
+        public int Attachments => ProcessMemory.HighPerfBitConverter.ToInt32(data, 0xA8);
+        public int Quantity => ProcessMemory.HighPerfBitConverter.ToInt32(data, 0xB0);
+
+        public bool IsEmptySlot => ItemID == 0 && WeaponID == -1;
+        public bool IsItem => ItemID != 0 && WeaponID == -1;
+        public bool IsWeapon => ItemID == 0 && WeaponID != -1;
+
+        public InventoryEntry(byte[] data)
+        {
+            this.data = data;
+        }
+    }
+}
